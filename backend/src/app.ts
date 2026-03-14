a62a175dfaa96272285c5ceb4ff94176862e181e
Franklin Mourao <beautyhair.faleconosco@gmail.com>
2026-03-13 21:06:33 -0300

    serve arquivos estaticos do widget

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express = require("express");
 import cors = require("cors");
+import path = require("path");
 import { router } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/widget", express.static(path.resolve(__dirname, "../widget")));
 
 app.get("/", (_req, res) => {
   return res.status(200).json({
